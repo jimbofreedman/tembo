86184d02eccd2273754aec8042cc32cb14c10a02
Jimbo Freedman <jimbo@jimbo.rocks>
2020-05-22 09:47:06 -0600

    Native sentry

--- native/App.tsx
+++ native/App.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import * as Sentry from 'sentry-expo';
+
+Sentry.init({
+    dsn: 'https://b09775658d1147bdac9f7eb190030070@o396764.ingest.sentry.io/5250612',
+    enableInExpoDevelopment: true,
+    debug: true
+});
+
 export default function App() {
     return (
         <View style={styles.container}>
