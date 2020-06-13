3c1c3dad9e15e9f189c7df5aab2ef8fd7b6985f9
Jimbo Freedman <jimbo@jimbo.rocks>
2020-06-13 15:04:17 -0600

    Web: Build with CircleCI

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
 
-import createBrowserHistory from 'history/createBrowserHistory';
-import { syncHistoryWithStore } from 'mobx-react-router';
+// import createBrowserHistory from 'history/createBrowserHistory';
+// import { syncHistoryWithStore } from 'mobx-react-router';
 import useStores from './hooks/useStores';
 import LoginScreen from './screens/LoginScreen';
 import { observer } from 'mobx-react';
 import HomeScreen from './screens/HomeScreen';
 
 function App() {
-    const { routingStore, authStore } = useStores();
-    const browserHistory = createBrowserHistory();
-    // const history = syncHistoryWithStore(browserHistory, routingStore);
+    const { /* routingStore, */ authStore } = useStores();
+//    const browserHistory = createBrowserHistory();
+//    const history = syncHistoryWithStore(browserHistory, routingStore);
 
     if (!authStore.isLoggedIn) {
         return <LoginScreen />;
